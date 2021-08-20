ac7d49c86d31e76f7109d1ff559b402469545a4a
antsareflying <seongmumoon@gmail.com>
2021-08-21 02:05:34 +0800

    Added exe to gitignore

--- src/opengltut.c
+++ src/opengltut.c
@@ -15,7 +15,7 @@ const char *fragment_shader_source = "#version 330 core\n"
     "}\0";
 
 const char *vertex_shader_source = "#version 330 core\n"
-    "layout(location = 0) in vec3 aPos;\n"
+    "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
