e67c1eb7786d08c113134c73441045d8109890eb
antsareflying <seongmumoon@gmail.com>
2021-08-21 12:22:01 +0800

    finished seond tutorial

--- src/opengltut.c
+++ src/opengltut.c
@@ -97,20 +97,32 @@ int main(void)
 
     float vertices[] = 
     {
+        0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
         -0.5f, -0.5f, 0.0f,
-        0.5f, 0.5f, 0.0f, 
-        0.0f, 0.0f, 0.0f
+        -0.5f, 0.5f, 0.0f
     };
 
-    unsigned int VBO, VAO;
+    unsigned int indices[] = 
+    {
+        0, 1, 3,
+        1, 2, 3
+    };
 
-    glGenBuffers(1, &VBO);
+    unsigned int VBO, VAO, EBO;
 
     glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -124,7 +136,8 @@ int main(void)
 
         glUseProgram(shader_program);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
